ca766e89d77f0bc24d6745679c1f37c96999bcde
HAKKI AYMAN <82755715+SoulCriminal@users.noreply.github.com>
2022-03-15 19:54:59 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+
+
+class Program 
+{
+    static void Main(string[] args)
+    {
+        //System.Collections namespace
+
+        ArrayList Liste= new ArrayList();
+        Liste.Add("Hakkı");
+        Liste.Add(2);
+        Liste.Add(true);
+        Liste.Add('A');
+
+        //Liste içerisindeki verilere erişim
+        System.Console.WriteLine(Liste[1]);
+
+        foreach (var item in Liste)
+        {
+            System.Console.WriteLine(item);            
+        }
+
+        //ARRAY LiST METOTLARI
+
+        //AddRange
+
+        System.Console.WriteLine("******** Add Range ********");
+        string[] renkler = {"Kırmızı","Sarı","Siyah"};
+        List<int> sayilar = new List<int>() {1,5,7,89,0};
+        
+        Liste.AddRange(renkler);
+        Liste.AddRange(sayilar);
+
+        foreach (var item in Liste)
+        {
+            System.Console.WriteLine(item);            
+        }
+
+        //Sort
+        System.Console.WriteLine("******** Sort ********");
+        Liste.Sort();// sıralarken dikkat etmemiz gerek listelerde farklı değerler tuttuğumuz için burada hata alırız.
+        //Sıralayabilmesi için tiplerin aynı olması gerekli.
+
+
+        //Binary Search
+        System.Console.WriteLine("******** Binary Search ********");
+        //Kullanmak için önce diziyi yada listeyi sıralamalı sonra bu metodu çalıştırmalıyız.
+
+        Console.WriteLine(Liste.BinarySearch(7));
+
+        //Reverse
+
+        System.Console.WriteLine("******** Reverse ********");
+        Liste.Reverse();
+        foreach (var item in Liste)
+        {
+            System.Console.WriteLine(item);
+        }
+
+        //Clear
+        System.Console.WriteLine("******** Clear ********");
+        Liste.Clear();
+
+        foreach (var item in Liste)
+        {
+            System.Console.WriteLine(item);
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,13 @@
+# Merhabalar C# ArrayList uygulamalı ve açıklamalı kodlarım!
+
+Merhabalar C# ile ilgili öğrenirken hazırladığım ArrayList ile ilgili kodlarımı içeren dosya. 
+
+
+# İçerik
+
+- Liste Oluşturmak
+-  Add Range 
+- Sort
+- Binary Search
+- Reverse
+- Clear
